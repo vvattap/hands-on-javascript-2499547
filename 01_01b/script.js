cedb251da5e180b9dc0f968527ef5c48ce76a7eb
Varsha Vattappally <63315944+vvattap@users.noreply.github.com>
2024-04-15 19:15:12 +0000

    breaking down a function

--- 01_01b/script.js
+++ 01_01b/script.js
@@ -9,16 +9,18 @@ import data from "./data.js";
 
 const mainContent = document.querySelector(".main-content");
 
-const Card = (data) => {
-  const imgData = data[0];
+const buildImage = (imgData) => {
+  let srcset = `${imgData.urls.full} ${imgData.width}w,`;
+  if (imgData.urls.regular) {
+    srcset = srcset + `,${imgData.urls.regular} 1080w,`;
+  }
+  if (imgData.urls.small) {
+    srcset = srcset + `,${imgData.urls.small} 400w`;
+  }
 
-  const markup = `
-    <figure class="image">
-      <img
+  const img = `<img
         srcset="
-          ${imgData.urls.full} ${imgData.width}w,
-          ${imgData.urls.regular} 1080w,
-          ${imgData.urls.small} 400w
+          ${srcset}
         "
         sizes="(max-width: 450px) 400px, (max-width: 800) 1080px"
         src="${imgData.urls.regular}"
@@ -26,13 +28,35 @@ const Card = (data) => {
         height="${imgData.height}"
         alt="${imgData.description}"
         loading="lazy"
-      />
+      />`;
+  return img;
+};
+const getDate = (imgData) => {
+  const date = new Date(imgData.created_at);
+  const niceDate = date.toLocaleString("default", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  return niceDate;
+};
+const Card = (data) => {
+  const imgData = data[0];
+
+  const markup = `
+    <figure class="image">
+      ${buildImage(imgData)}
       <figcaption class="image__caption">
         <h3 class="image__title">${imgData.description}</h3>
         <div class="image__meta">
           <p>
             Photo by
             <span class="image__photog">${imgData.user.name}</span>.
+          </p>
+          <p>
+          Uploaded On
+          <time class="image_date" dateTime="${imgData.created_at}"> 
+          ${getDate(imgData)} </time>
           </p>
           <p>
             <a href="${imgData.links.self}" class="image__link">
